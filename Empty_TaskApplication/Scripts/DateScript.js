0d3b5ea5de1d0210b4c3323deaccc5fc8425a22b
ashwinrrao <rashwinrao1993@gmail.com>
2017-04-14 17:11:33 +0530

    Add files via upload

---
+++ Empty_TaskApplication/Scripts/DateScript.js
@@ -0,0 +1,7 @@
+﻿function DateChangeEvent() {
+    var date = new Date(document.getElementById("date").value);
+    if (date.getDay() == 0 || date.getDay() == 6)
+        alert("Holiday");
+    else
+        alert("Working Day");
+}
